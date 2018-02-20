a46b32a3658cc26ca4eea19e23fbde84a15305f4
EzeVedder <pane.ezequiel@gmail.com>
2018-02-20 11:01:16 -0300

    20/02/2018----Ejercicio 6-7 ----ITERACIONES WHILE

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,12 +1,15 @@
 function Mostrar()
 {
-
 	var contador=0;
 	var acumulador=0;
-
-
-
+	var num;
+	while(contador<5)
+	{
+		contador ++;
+		num = prompt("Elija un número");
+		num = parseInt(num);
+		acumulador += num;
+	}
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/5;
-
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -1,12 +1,33 @@
 function Mostrar()
 {
-
+ 
+	//var respuesta='si';
 	var contador=0;
 	var acumulador=0;
-	var respuesta='si';
-
+	var num;
+	var rta=true;
+	//comienza a preguntar
+	while(rta == true)
+	{
+		num = prompt("Ingrese un número");
+		if(num == null)
+		//si cancelo salgo del while
+		{
+			break;
+		}
+		else if(isNaN(num)==true)
+		//valido que ingrese un número
+		{
+			alert("Entrada incorrecta, por favor ingrese un número válido");
+			continue
+		}
+		num = parseInt(num);
+		acumulador = acumulador + num;
+		contador++
+		rta = confirm("¿Desea seguir ingresando números?");
+	}
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
-
 }//FIN DE LA FUNCIÓN
+  
